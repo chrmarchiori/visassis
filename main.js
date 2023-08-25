26618e8eee4a43688768e96c6797660a6beca2c2
Chrystian Marchiori <chrmarchiori@hotmail.com>
2023-08-25 16:49:33 -0300

    Feat: Added color name input

--- main.js
+++ main.js
@@ -33,6 +33,45 @@ function rgbToHsl(r, g, b) {
   };
 }
 
+function getClosestColorName(r, g, b) {
+  const colorTable  = [
+    { name: "Vermelho", rgb: [255, 0, 0] },
+    { name: "Verde", rgb: [0, 255, 0] },
+    { name: "Azul", rgb: [0, 0, 255] },
+    { name: "Amarelo", rgb: [255, 255, 0] },
+    { name: "Roxo", rgb: [128, 0, 128] },
+    { name: "Laranja", rgb: [255, 165, 0] },
+    { name: "Rosa", rgb: [255, 192, 203] },
+    { name: "Marrom", rgb: [165, 42, 42] },
+    { name: "Cinza", rgb: [128, 128, 128] },
+    { name: "Preto", rgb: [0, 0, 0] },
+    { name: "Branco", rgb: [255, 255, 255] },
+    { name: "Ciano", rgb: [0, 255, 255] },
+    { name: "Magenta", rgb: [255, 0, 255] },
+    { name: "Turquesa", rgb: [64, 224, 208] },
+    { name: "Dourado", rgb: [255, 215, 0] },
+    { name: "Prateado", rgb: [192, 192, 192] },
+  ];
+
+  let closestColor = colorTable[0];
+  let minDistance = Number.MAX_VALUE;
+
+  for (const color of colorTable) {
+    const distance = Math.sqrt(
+      Math.pow(r - color.rgb[0], 2) +
+      Math.pow(g - color.rgb[1], 2) +
+      Math.pow(b - color.rgb[2], 2)
+    );
+
+    if (distance < minDistance) {
+      minDistance = distance;
+      closestColor = color;
+    }
+  }
+
+  return closestColor.name;
+}
+
 class App {
   $camera = document.querySelector("#camera");
   $canvas = document.querySelector("#canvas");
@@ -97,23 +136,22 @@ class App {
         imageCropSize.height
       );
       const averageColor = this.getAverageColor(data);
-
+      
       const rgb = `rgb(${averageColor.r},${averageColor.g},${averageColor.b})`;
       const hsl = rgbToHsl(averageColor.r, averageColor.g, averageColor.b);
-
+        
       document.body.style.backgroundColor = rgb;
 
       console.log(hsl);
 
-      const colorKey = [averageColor.r, averageColor.g, averageColor.b];
-      const color = colorName[colorKey];
-      const colorNameText = color ? color[1] : 'Cor não encontrada';
+      const colorName = getClosestColorName(averageColor.r, averageColor.g, averageColor.b)
 
-      this.say(colorNameText);
+      this.say(colorName);
     } catch (e) {
       console.log(e);
     }
   }
+  
 
   getAverageColor(imageData) {
     const blockSize = 5;
